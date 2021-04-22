d47a8beb6c6571d78fd9dcccceb0005881685252
Justin Nguyen <tin.nc1708@gmail.com>
2021-04-22 13:29:20 +1000

    Initial Commit

---
+++ Eateries/EateriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  EateriesApp.swift
+//  Eateries
+//
+//  Created by Nguyen Chanh Tin on 4/22/21.
+//
+
+import SwiftUI
+
+@main
+struct EateriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

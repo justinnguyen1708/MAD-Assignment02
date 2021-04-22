89b51579cb01f2a7816db5026a3d74d1656bab61
Justin Nguyen <tin.nc1708@gmail.com>
2021-04-22 13:44:24 +1000

    Add Eatery and EateryViewModel classes

---
+++ Eateries/Eatery.swift
@@ -0,0 +1,16 @@
+//
+//  Eatery.swift
+//  Eateries
+//
+//  Created by Nguyen Chanh Tin on 4/22/21.
+//
+
+import Foundation
+
+class Eatery {
+    var title: String
+    
+    init (title: String) {
+        self.title = title
+    }
+}
---
+++ Eateries/EateryViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  EateryViewModel.swift
+//  Eateries
+//
+//  Created by Nguyen Chanh Tin on 4/22/21.
+//
+
+import Foundation
+
+class EateryViewModel {
+    var listOfEateries: [Eatery]
+    
+    init(listOfEateries: [Eatery]) {
+        self.listOfEateries = listOfEateries
+    }
+}

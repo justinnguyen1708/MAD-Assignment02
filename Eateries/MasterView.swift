e8369b4e57748ef715c9f5bd81d1bb80cfda2324
Justin Nguyen <tin.nc1708@gmail.com>
2021-04-22 16:07:57 +1000

    Create MasterView, DetailView, and RowView

---
+++ Eateries/MasterView.swift
@@ -0,0 +1,20 @@
+//
+//  MasterView.swift
+//  Eateries
+//
+//  Created by Nguyen Chanh Tin on 4/22/21.
+//
+
+import SwiftUI
+
+struct MasterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MasterView_Previews: PreviewProvider {
+    static var previews: some View {
+        MasterView()
+    }
+}
---
+++ Eateries/RowView.swift
@@ -0,0 +1,20 @@
+//
+//  RowView.swift
+//  Eateries
+//
+//  Created by Nguyen Chanh Tin on 4/22/21.
+//
+
+import SwiftUI
+
+struct RowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RowView_Previews: PreviewProvider {
+    static var previews: some View {
+        RowView()
+    }
+}

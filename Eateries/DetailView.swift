0de077dad3a92ba71a73b2344d4fa66c2f3922a5
Justin Nguyen <tin.nc1708@gmail.com>
2021-04-29 14:58:39 +1000

    Modify DetailView for editing fields

--- Eateries/DetailView.swift
+++ Eateries/DetailView.swift
@@ -13,42 +13,95 @@ struct DetailView: View {
     
     var body: some View {
         List {
-            HStack {
-                    Spacer()
-                    eatery.downloadEateryImage(eateryURL: eatery.image)
-                        .resizable()
-                        .frame(width: 288.0, height: 256.0, alignment: .center)
-                        .cornerRadius(20)
-                        .shadow(radius: 200)
-                    Spacer()
-            }
-            // Eatery title, description and story are shown in this section
-            Section(header: Text(eatery.name)
-                        .font(.largeTitle)
-                        .fontWeight(.medium)
-            ){
-                Text(eatery.location)
-                    .font(.body)
-            }
-            
-            // Recipe section
-            Section (header: Text("NOTES")){
-                Text(eatery.notes)
-                    .font(.body)
-            }
-            
-            // Ingredients section
-            Section (header: Text("REVIEWS")){
-                ForEach(0..<eatery.reviews.count, id: \.self) { i in
-                    HStack {
-                        Text(eatery.reviews[i][0])
-                            .font(.body)
-                            .frame(width: 50.0)
+            if mode?.wrappedValue == .active {
+                HStack {
+                    TextField("Eatery image", text: $eatery.image)
+                }
+                // Eatery title, description and story are shown in this section
+                Section(header: TextField("Name", text: $eatery.name)){
+                    TextField("Location", text: $eatery.location)
+                }
+                
+                // Recipe section
+                Section (header: Text("NOTES")){
+                    TextField("Notes", text: $eatery.notes)
+                }
+                
+                // Ingredients section
+                Section (header: Text("REVIEWS")){
+                    ForEach(0..<eatery.reviews.count, id: \.self) { i in
+                        HStack {
+                            TextField("Author", text: $eatery.reviews[i][0])
+                                .font(.body)
+                                .frame(width: 50.0)
+                            Spacer()
+                                .frame(width: 50.0)
+                            TextField("Review", text: $eatery.reviews[i][1])
+                                .font(.body)
+                                .multilineTextAlignment(.leading)
+                        }
+                        // Delete a ingredient
+                    }.onDelete { indices in
+                        eatery.removeReview(at: indices)
+                        // Reorder a ingredient
+                    }.onMove { (indices, destination) in
+                        eatery.moveReview(from: indices, to: destination)
+                    }
+                    
+                    HStack{
                         Spacer()
-                            .frame(width: 50.0)
-                        Text(eatery.reviews[i][1])
-                            .font(.body)
-                            .multilineTextAlignment(.leading)
+                        Button(action: {}) {
+                            Image(systemName: "plus")
+                            .frame(width: 25, height: 25)
+                            .foregroundColor(Color.white)
+                            .background(Color.blue)
+                            .clipShape(Circle())
+                        }.onTapGesture {
+                            withAnimation{
+                                eatery.addNewReview()
+                            }
+                        }
+                        Spacer()
+                    }
+                }
+            } else {
+                HStack {
+                        Spacer()
+                        eatery.downloadEateryImage(eateryURL: eatery.image)
+                            .resizable()
+                            .frame(width: 288.0, height: 256.0, alignment: .center)
+                            .cornerRadius(20)
+                            .shadow(radius: 200)
+                        Spacer()
+                }
+                // Eatery title, description and story are shown in this section
+                Section(header: Text(eatery.name)
+                            .font(.largeTitle)
+                            .fontWeight(.medium)
+                ){
+                    Text(eatery.location)
+                        .font(.body)
+                }
+                
+                // Recipe section
+                Section (header: Text("NOTES")){
+                    Text(eatery.notes)
+                        .font(.body)
+                }
+                
+                // Ingredients section
+                Section (header: Text("REVIEWS")){
+                    ForEach(0..<eatery.reviews.count, id: \.self) { i in
+                        HStack {
+                            Text(eatery.reviews[i][0])
+                                .font(.body)
+                                .frame(width: 50.0)
+                            Spacer()
+                                .frame(width: 50.0)
+                            Text(eatery.reviews[i][1])
+                                .font(.body)
+                                .multilineTextAlignment(.leading)
+                        }
                     }
                 }
             }

c46f30f9a39191f9e1aee298b683c0bff67ebd5e
Justin Nguyen <tin.nc1708@gmail.com>
2021-05-13 15:42:41 +1000

    Add MasterView to ContentView

--- EateriesCoreData/ContentView.swift
+++ EateriesCoreData/ContentView.swift
@@ -8,66 +8,18 @@
 import SwiftUI
 import CoreData
 
+
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
 
     @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Eatery.name, ascending: true)],
+        sortDescriptors: [NSSortDescriptor(keyPath: \EateryModel.name, ascending: true)],
         animation: .default)
-    private var eateries: FetchedResults<Eatery>
-
+    private var eateryModel: FetchedResults<EateryModel>
+    
     var body: some View {
-        List {
-            ForEach(eateries) { eatery in
-                Text(eatery.imageString)
-                Text(eatery.nameString)
-                Text(eatery.locationString)
-                Text(eatery.notesString)
-            }
-            .onDelete(perform: deleteItems)
-        }
-        .toolbar {
-            #if os(iOS)
-            EditButton()
-            #endif
-
-            Button(action: addItem) {
-                Label("Add Item", systemImage: "plus")
-            }
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newEatery = Eatery(context: viewContext)
-            newEatery.image = "blank"
-            newEatery.name = "Eatery"
-            newEatery.location = "Location"
-            newEatery.notes = "Notes"
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { eateries[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
+        NavigationView {
+            MasterView(eateryModel: eateryModel.first ?? EateryModel(context: viewContext))
         }
     }
 }
---
+++ EateriesCoreData/MasterView.swift
@@ -0,0 +1,48 @@
+//
+//  MasterView.swift
+//  EateriesCoreData
+//
+//  Created by Nguyen Chanh Tin on 5/13/21.
+//
+
+import SwiftUI
+
+struct MasterView: View {
+    @Environment(\.managedObjectContext) private var viewContext
+
+    @ObservedObject var eateryModel: EateryModel
+    
+    var body: some View {
+        List {
+//            ForEach(eateryModel.eateriesArray) { eatery in
+//                NavigationLink(
+//                    destination: DetailView(eatery: eatery)
+//                        .navigationBarItems(trailing: EditButton()),
+//                    // Eatery item thumbnail
+//                    label: {
+//                        // This view is created to ensure information are updated
+//                        // in MasterView after modifying information in DetailView
+//                        RowView(eatery: eatery)
+//                    })
+//                // Delete a specific eatery
+//            }.onDelete { offsets in
+//                eateryModel.deleteEatery(offsets: offsets)
+//            }
+        }
+        .navigationBarTitle("Eateries")
+        .navigationBarItems(leading: EditButton(),
+                             trailing: Button(action: {
+                                 withAnimation{
+                                     eateryModel.addEatery()
+                                 }
+                             }) {
+                                 Image(systemName: "plus")
+                             })
+    }
+}
+
+//struct MasterView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        MasterView()
+//    }
+//}

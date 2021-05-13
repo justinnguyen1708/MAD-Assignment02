0d48d6b3b33d302a611d92c37694feb7d6d01f73
Justin Nguyen <tin.nc1708@gmail.com>
2021-05-13 16:42:54 +1000

    Update DetailView display

--- EateriesCoreData/DetailView.swift
+++ EateriesCoreData/DetailView.swift
@@ -13,11 +13,59 @@ struct DetailView: View {
     @ObservedObject var eatery: Eatery
     
     var body: some View {
-        Text(eatery.imageString)
-        Text(eatery.nameString)
-        Text(eatery.locationString)
-        Text(eatery.notesString)
-        
+        List {
+            HStack {
+                    Spacer()
+                    eatery.downloadEateryImage(eateryURL: eatery.imageString)
+                        .resizable()
+                        .frame(width: 288.0, height: 256.0, alignment: .center)
+                        .cornerRadius(20)
+                        .shadow(radius: 200)
+                    Spacer()
+            }
+            // Eatery title, description and story are shown in this section
+            Section(header: Text(eatery.nameString)
+                        .font(.largeTitle)
+                        .fontWeight(.medium)
+            ){
+                Text(eatery.locationString)
+                    .font(.body)
+            }
+            
+            // Notes section
+            Section (header: Text("NOTES")){
+                Text(eatery.notesString)
+                    .font(.body)
+            }
+            
+            // Review section
+            Section (header: HStack{
+                Text("REVIEWS")
+
+                Button(action: {
+                    withAnimation{
+                        eatery.addReview()
+                    }
+                }) {
+                    Image(systemName: "plus")
+                }
+            }){
+                ForEach(eatery.reviewsArray) { review in
+                    HStack {
+                        Text(review.authorString)
+                            .font(.body)
+                            .frame(width: 50.0)
+                        Spacer()
+                            .frame(width: 50.0)
+                        Text(review.commentString)
+                            .font(.body)
+                            .multilineTextAlignment(.leading)
+                    }
+                }.onDelete { offsets in
+                    eatery.deleteReview(offsets: offsets)
+                }
+            }
+        }
     }
 }
 

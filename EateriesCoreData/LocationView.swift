82728a100471c4599122aba3890203990a2489be
Justin Nguyen <tin.nc1708@gmail.com>
2021-05-27 13:43:03 +1000

    Add LocationView and MapView

---
+++ EateriesCoreData/LocationView.swift
@@ -0,0 +1,14 @@
+//
+//  LocationView.swift
+//  EateriesCoreData
+//
+//  Created by Nguyen Chanh Tin on 5/27/21.
+//
+
+import SwiftUI
+
+struct LocationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
---
+++ EateriesCoreData/MapView.swift
@@ -0,0 +1,14 @@
+//
+//  MapView.swift
+//  EateriesCoreData
+//
+//  Created by Nguyen Chanh Tin on 5/27/21.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
